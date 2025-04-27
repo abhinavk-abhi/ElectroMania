5fc9b63e6881d38778fe7892825376f9359db51b
abhinavk-abhi <abhinavkottyadi234@gmail.com>
2025-04-27 20:04:27 +0530

    started wishlist

--- controller/wishlistController.js
+++ controller/wishlistController.js
@@ -6,8 +6,52 @@ import Product from "../model/productModel.js"
 const addToWishlist = async (req,res)=>{
     try {
        
-        const {userId,productId,price,basePrice} = req.body;
+        const {userId,productId} = req.body;
+        console.log(req.body)
+
+        if(!userId){
+            return res.status(401).json({message : "Please login to continue."})
+        }
+
+        if(!productId){
+            return res.status(404).json({message : "Product not found."})
+        }
+
+        const wishlist = await Wishlist.findOne({userId : userId})
+
+      
+
+
+
+    
+
+    } catch (error) {
         
+    }
+}
+
+const loadWishlist = async (req,res)=>{
+    try {
+       if(!req.session.user){
+                  return res.redirect('/user/login')
+              }
+      
+              const userId = req.session.user?.id ?? req.session.user?._id ?? null ;
+      
+              const user = await User.findOne({_id : userId});
+
+              const wishlist = await Wishlist.findOne({userId : userId}).populate('products.productId')
+
+              if(wishlist){
+                return res.render('wishlist',{wishlist , user })
+              } else {
+                await Wishlist.findOneAndUpdate(
+                    {userId},
+                    {products : []},
+                    {upsert : true , new : true}
+                )
+                return res.render('wishlist' , {wishlist : null , user })
+              }
 
     } catch (error) {
         
--- model/wishlistModel.js
+++ model/wishlistModel.js
@@ -2,16 +2,17 @@ import mongoose, { mongo } from "mongoose";
 const {Schema} = mongoose;
 
 const wishlistModel = new Schema ({
-    productId : [{
+    userId : {
+        type : Schema.Types.ObjectId,
+        ref : "user",
+        required : true
+    },
+    products : [{
         type : Schema.Types.ObjectId,
         ref : "products" ,
         required : true
     }],
-    customerId : {
-        type : Schema.Types.ObjectId,
-        ref : "user",
-        required : true
-    }
+    
 },{timestamps : true})
 
 export default mongoose.model("wishlist",wishlistModel)

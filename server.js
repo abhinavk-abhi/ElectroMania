a2f1758ff010cddd79e1a901b504d0bc3de72ea1
abhinavk-abhi <abhinavkottyadi234@gmail.com>
2025-05-05 20:02:03 +0530

    changed theme of the shop page

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || "yourSecretKey",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: { 

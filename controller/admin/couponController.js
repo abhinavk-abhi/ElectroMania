f11781b38a094c528f0f77d5ee273da06d3311cf
abhinavk-abhi <abhinavkottyadi234@gmail.com>
2025-05-20 20:03:13 +0530

    added coupon page and add coupon fuctionaliity and working and edit coupon functionaloty

--- controller/admin/couponController.js
+++ controller/admin/couponController.js
@@ -32,18 +32,87 @@ const loadCoupons = async (req,res)=>{
 const addCoupon = async (req,res)=>{
     try {
         
-        const { addName , addCode , addDescription , addStartDate , addExpiryDate , addMinPrice , addOfferPrice , addUsageType } = req.body;
+        const { addName , addCode , addDescription , addStartDate , addExpiryDate , addMinPrice , addOfferPrice , addStatus , addUsageType } = req.body;
+
+        const coupon = await Coupon.findOne({name : addName})
+        if(coupon){
+            return res.status(400).json({ error : "Coupon in this name already exists."})
+        }
 
         if(new Date(addStartDate) >= new Date(addExpiryDate)){
             return res.status(401).json({error : "Expiry date should have to be greater than Start date"})
         }
 
+        if(parseFloat(addMinPrice) < parseFloat(addOfferPrice)){
+            return res.status(400).json({error :  "Minimum price must have to be greater than offer price."})
+        }
+
+        await Coupon.create({
+            name : addName,
+            code : addCode,
+            description : addDescription,
+            startDate : addStartDate,
+            expiryDate : addExpiryDate,
+            minPrice : addMinPrice,
+            offerPrice : addOfferPrice,
+            status : addStatus,
+            usageType : addUsageType
+        })
+
+         return res.status(200).json({ message: 'Coupon added successfully' });
     } catch (error) {
-        
+        return res.status(500).json({ error : "Internal server error"})
     }
 } 
 
+const editCoupon = async (req,res)=>{
+    try {
+        
+    const {orgName, editName, editCode, editDescription, editStartDate, editExpiryDate, editMinPrice, editOfferPrice, editStatus, editUsageType} = req.body;
+
+    // const coupon = await Coupon.findOne({name : editName})
+    // if(coupon){
+    // return res.status(400).json({ error : "Coupon in this name already exists."})
+    //     }
+
+    if(new Date(editStartDate) >= new Date(editExpiryDate)){
+        return res.status(400).json({ error : "Expiry date should have to be greater than Start date"})
+    }
+
+    if(parseFloat(editMinPrice) < parseFloat(editOfferPrice)){
+        return res.status(400).json({ error :"Minimum price must have to be greater than offer price." })
+    }
+
+    const coupon = await Coupon.findOneAndUpdate(
+        {name : orgName},
+        {
+            name : editName,
+            code : editCode,
+            description : editDescription,
+            startDate : editStartDate,
+            expiryDate : editExpiryDate,
+            minPrice : editMinPrice,
+            offerPrice : editOfferPrice,
+            status : editStatus,
+            usageType : editUsageType
+        },
+        { new : true }
+    )
+
+    if(coupon) {
+        return res.statur(200).json({ message : "Coupon updated successfully"})
+    } else {
+        return res.status(400).json({ error : "Coupon update failed"})
+    }
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error : "Internal server error" })
+    }
+}
+
 export default {
     loadCoupons,
-    addCoupon
+    addCoupon,
+    editCoupon
 }
--- routes/admin.js
+++ routes/admin.js
@@ -44,5 +44,6 @@ router.post('/orderReturn',adminAuth.checkSession,adminOrder.orderReturn)
 //Coupon
 router.get('/coupons',adminAuth.checkSession,couponController.loadCoupons)
 router.post('/coupons',adminAuth.checkSession,couponController.addCoupon)
+router.put('/coupons',adminAuth.checkSession,couponController.editCoupon)
 
 export default router;
